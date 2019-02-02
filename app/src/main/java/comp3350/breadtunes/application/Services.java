7c44b5d999a3085e559e2c03a26052944badbc2c
Daniel Lovegrove <lovegrod@myumanitoba.ca>
2019-02-02 15:43:54 -0600

    Fix build errors by removing sample project logic

--- app/src/main/java/comp3350/breadtunes/application/Services.java
+++ app/src/main/java/comp3350/breadtunes/application/Services.java
@@ -6,7 +6,7 @@ import comp3350.breadtunes.persistence.stubs.SongPersistenceStub;
 public class Services
 {
 	private static SongPersistence songPersistence = null;
-	
+
     public static synchronized SongPersistence getSongPersistence()
     {
         if (songPersistence == null)
