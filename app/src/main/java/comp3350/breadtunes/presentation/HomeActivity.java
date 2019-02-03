67048a05f26ad087284b096c21b0afd9cdd3fdf4
Mario M <mariomendez1234@live.com.mx>
2019-02-03 14:51:38 -0600

    add list that reads songs from database stubs and displays on main activity

--- app/src/main/java/comp3350/breadtunes/presentation/HomeActivity.java
+++ app/src/main/java/comp3350/breadtunes/presentation/HomeActivity.java
@@ -1,6 +1,10 @@
 package comp3350.breadtunes.presentation;
 
 import comp3350.breadtunes.R;
+import comp3350.breadtunes.application.Services;
+import comp3350.breadtunes.objects.Song;
+import comp3350.breadtunes.persistence.*;
+import comp3350.breadtunes.persistence.stubs.SongPersistenceStub;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,16 +13,40 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class HomeActivity extends Activity {
 
+
+    // To view info about the activity lifecycle https://developer.android.com/guide/components/activities/activity-lifecycle
+    // Populating lists with custom content https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
+
+        //Get the list of songs from application services
+        Services services = new Services();
+        SongPersistence songPersistenceStub = services.getSongPersistence(); //get interface for getting songs from persistance
+
+        List<Song> songList = songPersistenceStub.getAll(); //get all songs in persistance
+        String[] songNames = new String[songList.size()]; //array where all song names will be stored
+
+        for(int i= 0; i<songList.size(); i++){
+            songNames[i] = songList.get(i).getName();
+        }
+
+        //create adapter to populate list items in the listView in the main activity
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.songlist_element, songNames);
+        ListView activitySongList = (ListView) findViewById(R.id.songList);
+        activitySongList.setAdapter(adapter); //populate the items!
+
+    }//on create
 
     @Override
     protected void onDestroy() {
