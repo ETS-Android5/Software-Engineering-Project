8ad0518cb6c583259e9bea32d82d3b234235d83e
Daniel Lovegrove <lovegrod@myumanitoba.ca>
2019-02-02 20:05:12 -0600

    Remove unnecessary code from Command Line Interface

--- app/src/main/java/comp3350/breadtunes/presentation/CLI.java
+++ app/src/main/java/comp3350/breadtunes/presentation/CLI.java
@@ -18,16 +18,6 @@ public class CLI  // command-line interface
 	public static String inputLine;
 	public static String[] inputTokens;
 	
-	public static Student currentStudent;
-	public static Course currentCourse;
-	public static SC currentSC;
-	public static SC currentCS;
-
-	public static String studentNumber;
-	public static String courseNumber;
-	
-	public static String indent = "  ";
-	
 	public static void run()
 	{
 		try
@@ -85,139 +75,9 @@ public class CLI  // command-line interface
 
 	public static void processGet()
 	{
-		if (inputTokens[1].equalsIgnoreCase("Student"))
+		if (inputTokens[1].equalsIgnoreCase("Some command"))
 		{
-			processGetStudent();
-		}
-		else if (inputTokens[1].equalsIgnoreCase("Course"))
-		{
-			processGetCourse();
-		}
-		else if (inputTokens[1].equalsIgnoreCase("SC"))
-		{
-			processGetSC();
-		}
-		else if (inputTokens[1].equalsIgnoreCase("CS"))
-		{
-			processGetCS();
-		}
-		else
-		{
-			System.out.println("Invalid data type");
-		}
-	}
-
-	public static void processGetStudent()
-	{
-		AccessStudents accessStudents;
-		AccessSC accessSC;
-		
-		accessStudents = new AccessStudents();
-		
-		if (inputTokens.length > 2)
-		{
-			if (inputTokens[2].equalsIgnoreCase("orphan"))
-			{
-				accessSC = new AccessSC();
-				currentStudent = accessStudents.getSequential();			
-				while (currentStudent != null)
-				{
-					studentNumber = currentStudent.getStudentID();
-					accessSC = new AccessSC();
-					currentSC = accessSC.getSC(studentNumber);
-					if (currentSC == null)
-					{
-						System.out.println(indent +currentStudent);
-					}
-					currentStudent = accessStudents.getSequential();
-				}
-			}
-			else
-			{
-				studentNumber = inputTokens[2];
-				currentStudent = accessStudents.getRandom(studentNumber);
-				System.out.println(indent +currentStudent);
-			}
-		}
-		else
-		{
-			currentStudent = accessStudents.getSequential();			
-			while (currentStudent != null)
-			{
-				studentNumber = currentStudent.getStudentID();
-				System.out.println(indent +currentStudent);
-				currentStudent = accessStudents.getSequential();
-			}
-		}
-	}
-
-	public static void processGetCourse()
-	{
-		AccessCourses accessCourses;
-		AccessSC accessSC;
-
-		accessCourses = new AccessCourses();
-		
-		if (inputTokens.length > 2)
-		{
-			if (inputTokens[2].equalsIgnoreCase("orphan"))
-			{
-				accessSC = new AccessSC();
-				currentCourse = accessCourses.getSequential();			
-				while (currentCourse != null)
-				{
-					courseNumber = currentCourse.getCourseID();
-					accessSC = new AccessSC();
-					currentCS = accessSC.getCS(courseNumber);
-					if (currentCS == null)
-					{
-						System.out.println(indent +currentCourse);
-					}
-					currentCourse = accessCourses.getSequential();
-				}
-			}
-			else
-			{
-				courseNumber = (inputTokens[2]).toUpperCase(Locale.getDefault());
-				currentCourse = accessCourses.getRandom(courseNumber);
-				System.out.println(indent +currentCourse);
-			}
-		}
-		else
-		{
-			currentCourse = accessCourses.getSequential();			
-			while (currentCourse != null)
-			{
-				courseNumber = currentCourse.getCourseID();
-				System.out.println(indent +currentCourse);
-				currentCourse = accessCourses.getSequential();
-			}
-		}
-	}
-
-	public static void processGetSC()
-	{
-		AccessSC accessSC;
-		
-		accessSC = new AccessSC();
-		currentSC = accessSC.getSC(studentNumber);			
-		while (currentSC != null)
-		{
-			System.out.println(indent +currentSC);
-			currentSC = accessSC.getSC(studentNumber);
-		}
-	}
-
-	public static void processGetCS()
-	{
-		AccessSC accessSC;
-		
-		accessSC = new AccessSC();
-		currentCS = accessSC.getCS(courseNumber);			
-		while (currentCS != null)
-		{
-			System.out.println(indent +currentCS);
-			currentCS = accessSC.getCS(courseNumber);
+			// Do some processing
 		}
 	}
 }
