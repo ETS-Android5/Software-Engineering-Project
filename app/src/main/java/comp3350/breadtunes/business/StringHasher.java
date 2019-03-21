3b3446cf70f095b5a14f2512886a5eb83df98a10
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-03-21 15:43:49 -0500

    Workaround bug with Apache Commons DigestUtils

--- app/src/main/java/comp3350/breadtunes/business/StringHasher.java
+++ app/src/main/java/comp3350/breadtunes/business/StringHasher.java
@@ -1,9 +1,10 @@
 package comp3350.breadtunes.business;
 
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class StringHasher {
     public static String sha256HexHash(String str) {
-        return DigestUtils.sha256Hex(str);
+        return new String(Hex.encodeHex(DigestUtils.sha256(str)));
     }
 }
