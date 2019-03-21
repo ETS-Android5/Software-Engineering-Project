30d67fd2a62a8b6efc0ee4f520f619e3341887b4
Mario M <mariomendez1234@live.com.mx>
2019-03-21 11:36:45 -0500

    just the todo

--- app/src/main/java/comp3350/breadtunes/presentation/HomeActivity.java
+++ app/src/main/java/comp3350/breadtunes/presentation/HomeActivity.java
@@ -31,7 +31,7 @@ import java.util.Observer;
 //    // PLAY SONGS https://developer.android.com/guide/topics/media/mediaplayer#java
 //==============================
 
-@// TODO: 20/03/19 agregar accion para forgot pin, agregar cambios en music player state que represente si esta activado, agregar checksa credentials have been set in parental lock activation, agregar ui element que diga "PARENTAL LOCK ON/OFF" 
+//@// TODO: 20/03/19 agregar accion para forgot pin, agregar cambios en music player state que represente si esta activado, agregar checksa credentials have been set in parental lock activation, agregar ui element que diga "PARENTAL LOCK ON/OFF"
 
 
 public class HomeActivity extends BaseActivity implements Observer {
