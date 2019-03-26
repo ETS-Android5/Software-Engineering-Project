47fd261600350e09868b6731e6cff3161d72228f
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-03-26 12:22:30 -0500

    Move db creation queries into its own class

--- app/src/main/java/comp3350/breadtunes/persistence/DatabaseManager.java
+++ app/src/main/java/comp3350/breadtunes/persistence/DatabaseManager.java
@@ -67,14 +67,13 @@ public class DatabaseManager {
 
     private void createDatabase(Connection dbConnection) throws SQLException {
         dbConnection.setAutoCommit(false);
+
         Statement statement = dbConnection.createStatement();
-        statement.addBatch("CREATE MEMORY TABLE PUBLIC.SONGS(SONGID INTEGER NOT NULL PRIMARY KEY, NAME VARCHAR(256), YEAR INTEGER, TRACK INTEGER, DURATION VARCHAR(256), ARTISTID INTEGER, ARTISTNAME VARCHAR(256), ALBUMID INTEGER, ALBUMNAME VARCHAR(256), URI VARCHAR(256));");
-        statement.addBatch("CREATE MEMORY TABLE PUBLIC.CREDENTIALS(CID INTEGER IDENTITY PRIMARY KEY, PIN CHAR(64) NOT NULL, SECURITYQUESTION VARCHAR(80) NOT NULL, SECURITYQUESTIONANS CHAR(64) NOT NULL, DATEUPDATED VARCHAR(64));");
-        statement.addBatch("INSERT INTO SONGS VALUES (-10, 'Haydn Adagio', 2000, 1, 'H:0M:3S:1', -10, 'Joseph Haydn', -10, 'Classical Tunes', 'android.resource://comp3350.breadtunes/raw/adagio');");
-        statement.addBatch("INSERT INTO SONGS VALUES (-11, 'Jarabe Tapatio', 2000, 1, 'H:0M:3S:3', -11, 'Mexico', -11, 'Mexican Tunes', 'android.resource://comp3350.breadtunes/raw/jarabe');");
-        statement.addBatch("INSERT INTO SONGS VALUES (-12, 'Nocturne', 2000, 1, 'H:0M:3S:5', -12, 'Frederic Chopin', -12, 'Classical Tunes', 'android.resource://comp3350.breadtunes/raw/nocturne');");
-        statement.addBatch("INSERT INTO SONGS VALUES (-13, 'Bloch Prayer', 2000, 1, 'H:0M:3S:6', -13, 'Ernest Bloch', -13, 'Classical Tunes', 'android.resource://comp3350.breadtunes/raw/prayer');");
+        for (String query : DatabaseCreationQueries.createDb) {
+            statement.addBatch(query);
+        }
         statement.executeBatch();
+
         dbConnection.setAutoCommit(true);
 
         Log.i("HSQLDB", "Initialized database tables because database did not exist.");
