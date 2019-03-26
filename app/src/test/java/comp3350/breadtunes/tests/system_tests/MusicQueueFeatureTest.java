e8cc2eda87d3fb9b4eda0e91d8b740ca9f40c8f7
Mario M <mariomendez1234@live.com.mx>
2019-03-26 13:05:20 -0500

    added classes for system tests

---
+++ app/src/test/java/comp3350/breadtunes/tests/system_tests/MusicQueueFeatureTest.java
@@ -0,0 +1,11 @@
+package comp3350.breadtunes.tests.system_tests;
+
+public class MusicQueueFeatureTest {
+
+    /*
+
+    1. Add/remove from queue	As a user I want to be able to add and remove songs from the queue
+2. Play next	As a user I want to be able to choose a song to play next, overwriting the next song in the queue
+3. Reorder queue	As a user I want to be able to reorder the songs in the queue
+     */
+}
