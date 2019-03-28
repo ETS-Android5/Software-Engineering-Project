5a8c32572860845e6b5cc82887fc200a91e574c0
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-03-27 22:42:23 -0500

    Add string hasher tests to AllTests

--- app/src/test/java/comp3350/breadtunes/tests/AllTests.java
+++ app/src/test/java/comp3350/breadtunes/tests/AllTests.java
@@ -14,6 +14,7 @@ import comp3350.breadtunes.tests.objects.*;
         MusicPlayerStateTest.class,
         CredentialManagerTest.class,
         DateTimeHelperTest.class,
+        StringHasherTest.class,
 })
 public class AllTests {
 }
