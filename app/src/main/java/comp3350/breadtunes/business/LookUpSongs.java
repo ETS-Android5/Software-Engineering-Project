564d5a34d13566d588ddb8f745bc72b97d89bc73
Mario M <mariomendez1234@live.com.mx>
2019-03-27 20:36:35 -0500

    changed lookUP songs to use mpState, fixing various bugs when parental control mode was on

--- app/src/main/java/comp3350/breadtunes/business/LookUpSongs.java
+++ app/src/main/java/comp3350/breadtunes/business/LookUpSongs.java
@@ -10,25 +10,13 @@ import comp3350.breadtunes.objects.Artist;
 import comp3350.breadtunes.objects.Song;
 
 public class LookUpSongs {
-    List<Song> allSongs;
-    List<Album> allAlbums;
-    List<Artist> allArtists;
 
-    public LookUpSongs(List<Song> songs, List<Album> albums, List<Artist> artists) {
-        allSongs = songs;
-        allAlbums = albums;
-        allArtists = artists;
-    }
-
-    public LookUpSongs(List<Song> songs){
-        allSongs = songs;
-    }
 
     public List<Song> searchSongs(String input) {
         List<Song> matchingSongs = new ArrayList<>();
 
-        for (int i = 0; i < allSongs.size(); i++) {
-            Song ss = allSongs.get(i);
+        for (int i = 0; i < MusicPlayerState.getInstance().getCurrentSongList().size(); i++) {
+            Song ss = MusicPlayerState.getInstance().getCurrentSongList().get(i);
 
             if (input.length() != 0 && ss.getName().toUpperCase().contains(input.toUpperCase())) {
                 matchingSongs.add(ss);
@@ -36,32 +24,6 @@ public class LookUpSongs {
         }
 
         return matchingSongs;
-    }
-
-    public List<Album> searchAlbums(String input){
-        List<Album> matchingAlbums = new ArrayList<>();
-
-        for (int i = 0; i < allAlbums.size(); i++) {
-            Album al = allAlbums.get(i);
-
-            if (input.length() != 0 && al.getName().toUpperCase().contains(input.toUpperCase())) {
-                matchingAlbums.add(al);
-            }
-        }
-        return matchingAlbums;
-    }
-
-    public List<Artist> searchArtists(String input){
-        List<Artist> matchingArtists = new ArrayList<>();
-
-        for(int i = 0; i < allArtists.size(); i++) {
-            Artist ar = allArtists.get(i);
-
-            if (input.length() != 0 && ar.getName().toUpperCase().contains(input.toUpperCase())){
-                matchingArtists.add(ar);
-            }
-        }
-        return matchingArtists;
     }
 
     // DO NOT REMOVE, it is used; talk to mario if questions
