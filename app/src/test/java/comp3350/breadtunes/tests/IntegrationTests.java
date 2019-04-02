1f76de4b9b1c9202e6ac135bdd231fa7ab80da7e
Raven Carencia <carencrm@myumanitoba.ca>
2019-04-02 13:51:20 -0500

    add MusicPlayerStateIT

--- app/src/test/java/comp3350/breadtunes/tests/IntegrationTests.java
+++ app/src/test/java/comp3350/breadtunes/tests/IntegrationTests.java
@@ -4,10 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import comp3350.breadtunes.tests.business.LookUpSongsIT;
+import comp3350.breadtunes.tests.business.MusicPlayerStateIT;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        LookUpSongsIT.class
+        LookUpSongsIT.class,
+        MusicPlayerStateIT.class
 })
 public class IntegrationTests {
 }
